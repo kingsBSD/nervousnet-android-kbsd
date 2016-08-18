901a9654dd64509a0fe58c9e702a40fd211db548
Giles Richard Greenway <augeas@gmail.com>
2016-08-18 14:42:08 +0100

    More interlude... 	modified:   MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/ui/fragments/LocationFragment.java

--- MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/ui/fragments/LocationFragment.java
+++ MobileClients/Android/nervousnet/nervousnetHUB/src/main/java/ch/ethz/coss/nervousnet/hub/ui/fragments/LocationFragment.java
@@ -33,7 +33,6 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v13.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
